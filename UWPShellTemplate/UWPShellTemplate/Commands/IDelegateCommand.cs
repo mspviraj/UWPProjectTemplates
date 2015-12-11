d9059bd2f02be7b0971978788b5ba41b78920331
ronlemire2 <ronlemire2@gmail.com>
2015-12-11 08:40:24 -0800

    Add UWPShellTemplate solution

---
+++ UWPShellTemplate/UWPShellTemplate/Commands/IDelegateCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UWPShellTemplate.Commands {
+    public interface IDelegateCommand : ICommand {
+        void RaiseCanExecuteChanged();
+    }
+}
---
+++ UWPShellTemplate/UWPShellTemplate/Converters/FirstErrorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UWPShellTemplate.Converters {
+    /// <summary>
+    /// Value converter that retrieves the first error of the collection, or null if empty.
+    /// </summary>
+    public sealed class FirstErrorConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            ICollection<string> errors = value as ICollection<string>;
+            return errors != null && errors.Count > 0 ? errors.ElementAt(0) : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
+    }
+}
