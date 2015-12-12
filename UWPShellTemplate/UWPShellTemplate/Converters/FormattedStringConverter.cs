d5c7361d72c0de1bf027cb0bbe9e61cf128e2398
ronlemire2 <ronlemire2@gmail.com>
2015-12-12 07:40:22 -0800

    Added FormattedStringConverter. AutoSuggestBox under HamburgerButton for narrow width.

---
+++ UWPShellTemplate/UWPShellTemplate/Converters/FormattedStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UWPShellTemplate.Converters {
+    public class FormattedStringConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            if (value is IFormattable &&
+                parameter is string &&
+                !String.IsNullOrEmpty(parameter as string) &&
+                targetType == typeof(string)) {
+                if (String.IsNullOrEmpty(language))
+                    return (value as IFormattable).ToString(parameter as string, null);
+
+                return (value as IFormattable).ToString(parameter as string, new CultureInfo(language));
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
+    }
+}
