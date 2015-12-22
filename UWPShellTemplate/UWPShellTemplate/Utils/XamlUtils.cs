db8cd030c51264c9723b5c4efd2ce844172a02ee
ronlemire2 <ronlemire2@gmail.com>
2015-12-22 08:08:57 -0800

    Added XamlUtils.FindVisualChild utility method.

---
+++ UWPShellTemplate/UWPShellTemplate/Utils/XamlUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace UWPShellTemplate.Utils {
+    public class XamlUtils {
+        public static childItem FindVisualChild<childItem>(DependencyObject obj) where childItem : DependencyObject {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++) {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is childItem) {
+                    return (childItem)child;
+                }
+                else {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null) {
+                        return childOfChild;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
