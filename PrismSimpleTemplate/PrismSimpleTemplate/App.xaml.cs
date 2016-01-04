067c193cabbd2d8dc20d1b7d99f6a840cfe5a3de
ronlemire2 <ronlemire2@gmail.com>
2016-01-03 17:50:48 -0800

    Added PrismSimpleTemplate.

---
+++ PrismSimpleTemplate/PrismSimpleTemplate/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using PrismSimpleTemplate.Views;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Prism.Unity.Windows;
+using System.Threading.Tasks;
+
+namespace PrismSimpleTemplate {
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : PrismUnityApplication {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App() {
+            this.InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args) {
+            NavigationService.Navigate("Main", null);
+            return Task.FromResult<object>(null);
+        }
+
+        protected override void ConfigureContainer() {
+            base.ConfigureContainer();
+            //RegisterTypeIfMissing(typeof(IFakeService), typeof(FakeService), true);
+        }
+    }
+}
---
+++ PrismSimpleTemplate/PrismSimpleTemplate/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismSimpleTemplate.ViewModels {
+    public class MainPageViewModel : ViewModelBase {
+        private string appName;
+
+        public MainPageViewModel() {
+            AppName = "PrismSimpleTemplate";
+        }
+
+        public string AppName {
+            get { return appName; }
+            set {
+                SetProperty<string>(ref appName, value);
+            }
+        }
+    }
+}
