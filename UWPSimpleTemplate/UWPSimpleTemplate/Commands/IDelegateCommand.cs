3a7063084f3f7ab1f285a267f804fc3dfeb0a37c
ronlemire2 <ronlemire2@gmail.com>
2016-01-04 16:43:31 -0800

    Added basic commanding.

---
+++ UWPSimpleTemplate/UWPSimpleTemplate/Commands/IDelegateCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UWPSimpleTemplate.Commands {
+    public interface IDelegateCommand : ICommand {
+        void RaiseCanExecuteChanged();
+    }
+}
--- UWPSimpleTemplate/UWPSimpleTemplate/ViewModels/MainPageViewModel.cs
+++ UWPSimpleTemplate/UWPSimpleTemplate/ViewModels/MainPageViewModel.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UWPSimpleTemplate.Commands;
 
 namespace UWPSimpleTemplate.ViewModels {
     public class MainPageViewModel : BindableBase {
         private string appName;
+        private string saveAppName;
+        public DelegateCommand ClickMeCommand { get; set; }
+
+        public MainPageViewModel() {
+            saveAppName = App.Current.Resources["AppName"].ToString();
+            ClickMeCommand = new DelegateCommand(ExecuteClickMeCommand, CanExecuteClickMeCommand);
+        }
+
         public string AppName {
             get { return appName; }
             set {
                 SetProperty<string>(ref appName, value);
             }
+        }
+
+        private void ExecuteClickMeCommand(object args) {
+            AppName = AppName == saveAppName ? "Hello UWP!" : saveAppName;
+        }
+
+        private bool CanExecuteClickMeCommand(object args) {
+            return true;
         }
     }
 }
